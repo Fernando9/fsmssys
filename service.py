8e2870d7f48f777255717508e04b51b4a00f58d2
Fernando Munoz <fernandohmunoz@gmail.com>
2016-11-01 12:03:52 -0500

    First commit

---
+++ service.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, abort, Response
+from twilio.rest import TwilioRestClient
+from twilio import TwilioRestException
+import json
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read('twi.config')
+account_sid = config['DEFAULT']['account_sid']
+auth_token = config['DEFAULT']['auth_token']
+from_ = config['DEFAULT']['from_']
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_route():
+    data = {
+        'Status':'Server is Running'
+    }
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/sms', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    if not request.json:
+        abort(400)
+    data = request.get_json()
+    to = data["to"]
+    body = data["body"]
+    client = TwilioRestClient(account_sid, auth_token)
+    try:
+        message_response = client.messages.create(to=to, from_=from_, body=body)
+        reply_dict = {}
+        reply_dict['msg_type'] = 'Message Sent'
+        reply_dict['account_sid'] = message_response.sid
+        reply_dict['date_sent'] = str(message_response.date_created)
+        reply_dict['status'] = 'Success'
+        js = json.dumps(reply_dict)
+        resp = Response(js, status=200, mimetype='application/json')
+    except TwilioRestException as e:
+        jse = json.dumps(e.msg)
+        resp = Response(jse, status=404, mimetype='application/json')
+    return resp
+
+@app.route('/response', methods=['GET'])
+def reply_message():
+    resp = twilio.twiml.Response()
+    resp.message("Hello There!!!!")
+    return str(resp)
+
+# if __name__ == "__main__":
+    # app.run(host='0.0.0.0')
+    # app.run(debug=True)
